67fc0bc57f34e8bc2eefe7d75e2d3db29a784d29
cashcoder88 <cashglobe@gmail.com>
2019-05-16 15:21:50 -0600

    created skeleton reducer

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,15 @@
+
+// import ACTIONS
+
+
+
+const initialState = {
+    friends: []
+}
+
+export const reducer = (state = initialState, action) => {
+    console.log(state)
+    switch(action.type) {
+
+    }
+}
